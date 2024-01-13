e3d7c4849334172c666fa47a36631bb828130c0d
ka1ut <tka1utjp@gmail.com>
2024-01-14 01:51:29 +0900

    feat Home page

--- RecoGrapher/View/Screen/Home/Home.swift
+++ RecoGrapher/View/Screen/Home/Home.swift
@@ -1,15 +1,25 @@
-//
-//  Home.swift
-//  RecoGrapher
-//
-//  Created by 田中魁 on 2024/01/14.
-//
-
 import SwiftUI
 
 struct Home: View {
+    @State var selection = 1
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            GeometryReader { geometry in
+                VStack {
+                    HomeHeaderView()
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        RecordButton()
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 26.0, trailing: 25.0))
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            }
+        }
     }
 }
 
