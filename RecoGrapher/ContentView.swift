63f54fd8bbd928aad7fdbb67a6447b520fcdb512
ka1ut <tka1utjp@gmail.com>
2024-01-14 22:18:43 +0900

    feat アプリ起動時にアクセス権限

--- RecoGrapher/ContentView.swift
+++ RecoGrapher/ContentView.swift
@@ -1,32 +1,54 @@
 import SwiftUI
-import Speech
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    var speechRecorder = SpeechRecognitionEngine()
+    
     var body: some View {
         ZStack {
             Home()
                 .onAppear {
                     requestSpeechAuthorization()
                 }
+                .alert(isPresented: $showingAlert) {
+                    Alert(
+                        title: Text("アクセス権限が必要"),
+                        message: Text("設定画面よりアクセス権限を設定してください。"),
+                        dismissButton: .default(Text("設定を開く"), action: {
+                            // アプリの設定ページを開く
+                            if let url = URL(string: UIApplication.openSettingsURLString),
+                               UIApplication.shared.canOpenURL(url) {
+                                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                            }
+                        })
+                    )
+                }
         }
     }
+
     private func requestSpeechAuthorization() {
-        SFSpeechRecognizer.requestAuthorization { authStatus in
-            switch authStatus {
-            case .authorized:
-                print("音声認識へのアクセスが許可されました。")
-            case .denied:
-                print("音声認識へのアクセスが拒否されました。")
-            case .restricted:
-                print("このデバイスでは音声認識が制限されています。")
-            case .notDetermined:
-                print("音声認識へのアクセスがまだ決定されていません。")
-            @unknown default:
-                print("未知の認証ステータス")
+        if !speechRecorder.authorization() {
+            // マイク認識要求
+            speechRecorder.requestAccess() {
+                if speechRecorder.authorization() {
+                    // 正常時処理
+                    print("Authorization OK")
+                } else {
+                    // 異常時処理
+                    print("Authorization NG")
+                    DispatchQueue.main.async {
+                        // アラートを表示するための状態を更新
+                        showingAlert = true
+                    }
+                }
             }
         }
+        
+        
     }
 }
+
 
 #Preview {
     ContentView()
