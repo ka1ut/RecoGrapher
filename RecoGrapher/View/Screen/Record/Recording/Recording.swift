440465b10e78611229a2b5f3184880e2cc9f5d1f
ka1ut <tka1utjp@gmail.com>
2024-01-15 17:45:20 +0900

    feat scrollview

--- RecoGrapher/View/Screen/Record/Recording/Recording.swift
+++ RecoGrapher/View/Screen/Record/Recording/Recording.swift
@@ -11,69 +11,47 @@ struct RecordingPage: View {
     @ObservedObject private var recordingViewModel = RecordingViewModel()
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {
-                    dismiss()
-                }, label: {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(Color.customBlack)
-                })
-                
-                Spacer()
-                
-                Text("Recording")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.customBlack)
-                
-                Spacer()
-                
-                Button(action: {
-                }, label: {
-                    Image(systemName: "ellipsis")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20)
-                        .foregroundColor(Color.customBlack)
-                })
-                
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(15)
-            
-            ZStack{
-                // 背景
-                VStack() {
-                    Button(action:{
-                        recordingViewModel.onTaped()
-                    },label:{
-                        Image(systemName: recordingViewModel.isRecognition ? "mic.slash.circle" : "mic.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(recordingViewModel.isRecognition ? .red : .primary)
-                    })
+        GeometryReader { geometryProxy in
+
+                VStack {
+                    RecordHeaderView()
                     
-                    // 他のテキストやビューのコンテンツ
+                    ZStack{
+                        VStack() {
+                            ScrollView(.vertical){
+                                LazyVStack{
+                                    Image("person")
+                                    Image("person")
+                                }
+                            }
+                            
+                            
+                            Spacer()
+                            
+                            Button(action:{
+                                recordingViewModel.onTaped()
+                            },label:{
+                                Image(systemName: recordingViewModel.isRecognition ? "mic.slash.circle" : "mic.circle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .foregroundColor(recordingViewModel.isRecognition ? .red : .primary)
+                            })
+                        }
+                        
+                    }
                 }
-                .padding()
+                VStack{
+                    
+                    Spacer()
+                    
+                }
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
+                .background(Color.customWhite)
             }
-        }
-        VStack{
-            
-            Spacer()
-            
-        }
-        .padding()
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
-        .background(Color.customWhite)
     }
 }
-    
-    
+
 #Preview {
     RecordingPage()
 }
